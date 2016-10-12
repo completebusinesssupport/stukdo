ffc4171bb881041384a3b4e35014c1ebe76f39b2
Lorna Marlow <lorna@completebusinesssupport.co.uk>
2016-10-12 12:06:53 +0000

    Updated Form styling and added logic after sign in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
   def home
+    if current_user
+  		redirect_to tasks_path
+  	end
   end
-  
+
   def about
-    
+
   end
-  
+
   def change
-    
+
   end
 end
