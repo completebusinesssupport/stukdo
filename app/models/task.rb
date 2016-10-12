35c12256e5f0595c1ed5de1977695b17411a697d
Lorna Marlow <lorna@completebusinesssupport.co.uk>
2016-10-12 12:16:23 +0000

    Added server side validations on the tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :user
-  auto_html_for :content do 
+  validates :user_id, presence: true 
+	validates :content, presence: true 
+  
+  auto_html_for :content do
 		html_escape
 		image
 		youtube(width: '100%', height: 250 )
-		link target: '_blank', rel: 'nofollow' 
+		link target: '_blank', rel: 'nofollow'
 		simple_format
 	end
 end
